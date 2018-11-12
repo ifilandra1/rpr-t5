719a01066f29dc87ded63b9bee02ca9de3296464
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-11-12 10:19:08 +0100

    Pokusavao dodati onAction parametar u bilo koji button, ispisivalo gresku no controller specified for top level element. Kada pritisnem alt+enter, ne predlaze se kreiranje nove funkcije. Kada kreiram svoju funkciju onakvu kakvu bi trebao intellij, i dalje ne radi

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,5 +1,18 @@
 package ba.unsa.etf.rpr.tutorijal05;
+import javafx.event.ActionEvent;
+
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
+
 
 public class Controller {
+
 
 }
